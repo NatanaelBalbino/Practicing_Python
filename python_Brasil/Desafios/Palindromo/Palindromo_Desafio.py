e3dbc76422c4576d693f448392b2d9f09c14b53d
Natanael M M Balbino <49923456+NatanaelBalbino@users.noreply.github.com>
2023-10-01 11:58:43 -0300

    Update Palindromo_Desafio.py
    Variável refatorada e adição de conversão da string recebida para lower case

--- python_Brasil/Desafios/Palindromo/Palindromo_Desafio.py
+++ python_Brasil/Desafios/Palindromo/Palindromo_Desafio.py
@@ -8,15 +8,15 @@ print("-" * 50)
 palavra_original = input("Informe: ")
 palavra_semEspacos = palavra_original.replace(' ', '')
 
-# Remover os Acentos
-palavra_semAcentuacao = unidecode(palavra_semEspacos)
+# Remover os Acentos e converte para lower case
+palavra_semAcentuacao = unidecode(palavra_semEspacos).lower()
 
 # Remove as Pontuaçoes
-palavra_semAcentuacao = palavra_semAcentuacao.translate(str.maketrans('', '', string.punctuation))
+palavra_semPontuacao = palavra_semAcentuacao.translate(str.maketrans('', '', string.punctuation))
 
-palavra_virada = palavra_semAcentuacao[::-1]
+palavra_virada = palavra_semPontuacao[::-1]
 
-if palavra_virada == palavra_semAcentuacao:
+if palavra_virada == palavra_semPontuacao:
     print(f'"{palavra_original}" é um palíndromo!')
 else:
     print(f'"{palavra_original}" não é um palíndromo!')
